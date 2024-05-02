558d5f2f54a73116e92fd53853a333c99a581a9a
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-02 13:56:18 +0200

    fiber setup with listen address from flag and api versioning

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	listenAddr := flag.String("listenAddr", "localhost:5000", "Listen address of the api server")
+	flag.Parse()
 
 	app := fiber.New()
 
-	app.Get("/foo", handleFoo)
+	apiV1 := app.Group("/api/v1")
 
-	app.Listen("localhost:5000")
+	app.Get("/foo", handleFoo)
+	apiV1.Get("/user", handleUser)
+
+	app.Listen(*listenAddr)
 
 }
 
@@ -18,6 +25,14 @@ func handleFoo(c *fiber.Ctx) error {
 	return c.JSON(
 		map[string]string{
 			"msg": "Working just fine",
+		},
+	)
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(
+		map[string]string{
+			"user": "Some user",
 		},
 	)
 }
