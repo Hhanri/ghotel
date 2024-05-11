b73a42c8b33d3080a474f454a3a1a599d3c12ae5
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-11 13:30:57 +0200

    [hotel] seed script

---
+++ scripts/seed.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/hhanri/ghotel/db"
+	"github.com/hhanri/ghotel/types"
+)
+
+func main() {
+
+	dbUri := flag.String("dbUri", "mongodb://localhost:27017", "DB Uri")
+	flag.Parse()
+
+	client, err := db.NewMongoClient(*dbUri)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hotelStore := db.NewMongoHotelStore(client, db.TestDBNAME)
+	roomStore := db.NewMongoRoomStore(client, db.TestDBNAME, hotelStore)
+
+	hotel := &types.Hotel{
+		Name:     "Belluga",
+		Location: "France",
+		Rooms:    []string{},
+	}
+
+	roomA := &types.Room{
+		Type:      types.SinglePersonRoomType,
+		BasePrice: 99.99,
+	}
+	roomB := &types.Room{
+		Type:      types.DeluxeRoomType,
+		BasePrice: 179.99,
+	}
+
+	insertHotelAndRooms(
+		hotelStore,
+		roomStore,
+		hotel,
+		[]*types.Room{
+			roomA,
+			roomB,
+		},
+	)
+
+}
+
+func insertHotelAndRooms(
+	hotelStore db.HotelStore,
+	roomStore db.RoomStore,
+	hotel *types.Hotel,
+	rooms []*types.Room,
+) error {
+	ctx := context.Background()
+
+	insertedHotel, err := hotelStore.Insert(ctx, hotel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, room := range rooms {
+		room.HotelID = insertedHotel.ID
+		_, err := roomStore.Insert(ctx, room)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Inserted %+v\n", room)
+	}
+
+	return nil
+}
