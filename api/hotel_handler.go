3a43fdb9a9c98877a1e9e89d88d68f29dcf3dc87
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-11 14:46:26 +0200

    [hotel] hotel handler get hotels

---
+++ api/hotel_handler.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/hhanri/ghotel/db"
+)
+
+type HotelHandler struct {
+	hotelStore db.HotelStore
+	roomStore  db.RoomStore
+}
+
+func NewHotelHandler(hs db.HotelStore, rs db.RoomStore) *HotelHandler {
+	return &HotelHandler{
+		hotelStore: hs,
+		roomStore:  rs,
+	}
+}
+
+func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
+
+	hotels, err := h.hotelStore.List(c.Context(), struct{}{})
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(hotels)
+
+}
