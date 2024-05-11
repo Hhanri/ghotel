b81da02ae87aff0000101a4b7f34cde1716c8b0c
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-11 13:13:15 +0200

    [hotel] renamed InsertRoom to Insert, added AddRoom method

--- db/hotel_store.go
+++ db/hotel_store.go
@@ -4,13 +4,15 @@ import (
 	"context"
 
 	"github.com/hhanri/ghotel/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 const hotelsColl = "hotels"
 
 type HotelStore interface {
-	InsertHotel(context.Context, *types.Hotel) (*types.Hotel, error)
+	Insert(context.Context, *types.Hotel) (*types.Hotel, error)
+	AddRoom(context.Context, *types.Room) error
 }
 
 type MongoHotelStore struct {
@@ -26,7 +28,7 @@ func NewMongoHotelStore(c *mongo.Client, dbName string) *MongoHotelStore {
 	}
 }
 
-func (s *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (*types.Hotel, error) {
+func (s *MongoHotelStore) Insert(ctx context.Context, hotel *types.Hotel) (*types.Hotel, error) {
 	res, err := s.coll.InsertOne(ctx, hotel)
 	if err != nil {
 		return nil, err
@@ -34,4 +36,25 @@ func (s *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (
 
 	hotel.ID = ObjectIdToString(res.InsertedID)
 	return hotel, nil
+}
+
+func (s *MongoHotelStore) AddRoom(ctx context.Context, room *types.Room) error {
+	hotelID, err := ToObjectID(room.HotelID)
+	if err != nil {
+		return err
+	}
+
+	roomID, err := ToObjectID(room.ID)
+	if err != nil {
+		return err
+	}
+
+	update := bson.M{
+		"$push": bson.M{
+			"rooms": roomID,
+		},
+	}
+
+	_, err = s.coll.UpdateByID(ctx, hotelID, update)
+	return err
 }
