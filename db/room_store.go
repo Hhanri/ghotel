b1594ac0369ec0a9a920059f138349dd10dcc0ba
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-13 14:35:46 +0200

    [hotel] room store GetRooms and GetRoomsByID

--- db/room_store.go
+++ db/room_store.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/hhanri/ghotel/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -11,6 +12,8 @@ const roomsColl = "rooms"
 
 type RoomStore interface {
 	Insert(context.Context, *types.Room) (*types.Room, error)
+	GetRooms(context.Context, interface{}) ([]*types.Room, error)
+	GetRoomsByID(context.Context, string) ([]*types.Room, error)
 }
 
 type MongoRoomStore struct {
@@ -42,6 +45,23 @@ func (s *MongoRoomStore) Insert(ctx context.Context, room *types.Room) (*types.R
 	}
 
 	return room, nil
+}
+
+func (s *MongoRoomStore) GetRooms(ctx context.Context, filter interface{}) ([]*types.Room, error) {
+	res, err := s.coll.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	var rooms []*types.Room
+	if err := res.All(ctx, &rooms); err != nil {
+		return nil, err
+	}
+	return rooms, nil
+}
+
+func (s *MongoRoomStore) GetRoomsByID(ctx context.Context, id string) ([]*types.Room, error) {
+	return s.GetRooms(ctx, bson.M{"hotelId": id})
 }
 
 func (s *MongoRoomStore) Drop(ctx context.Context) error {
