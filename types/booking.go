cf4e6ee85121a6d81f5b1fb47887c715eaf4e015
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-16 15:45:21 +0200

    [booking] type booking fixed numPeople being string instead of int

--- types/booking.go
+++ types/booking.go
@@ -6,7 +6,7 @@ type Booking struct {
 	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
 	RoomdID   string    `bson:"roomId" json:"roomId"`
 	UserID    string    `bson:"userId" json:"userId"`
-	NumPeople string    `bson:"numPeople" json:"numPeople"`
+	NumPeople int       `bson:"numPeople" json:"numPeople"`
 	FromDate  time.Time `bson:"fromDate" json:"fromDate"`
 	UntilDate time.Time `bson:"untilDate" json:"untilDate"`
 }
