aee370efe5a357483d604d0350c09d2e00e29795
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-16 14:59:37 +0200

    [booking] booking type

---
+++ types/booking.go
@@ -0,0 +1,12 @@
+package types
+
+import "time"
+
+type Booking struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	RoomdID   string    `bson:"roomId" json:"roomId"`
+	UserID    string    `bson:"userId" json:"userId"`
+	NumPeople string    `bson:"numPeople" json:"numPeople"`
+	FromDate  time.Time `bson:"fromDate" json:"fromDate"`
+	UntilDate time.Time `bson:"untilDate" json:"untilDate"`
+}
