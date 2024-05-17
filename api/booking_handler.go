5d3b9b0f3230fe5fb186984fa1dc0374030b65fb
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-17 13:00:51 +0200

    [booking] booking handler and HandleGetBookings

---
+++ api/booking_handler.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/hhanri/ghotel/db"
+)
+
+type BookingHandler struct {
+	store *db.Store
+}
+
+func NewBookingHandler(store *db.Store) *BookingHandler {
+	return &BookingHandler{
+		store: store,
+	}
+}
+
+func (h *BookingHandler) HandleGetBookings(c *fiber.Ctx) error {
+	bookings, err := h.store.Booking.GetAll(c.Context(), struct{}{})
+	if err != nil {
+		return FiberInternalErrorResponse(c)
+	}
+	return c.JSON(bookings)
+}
+
+func (h *BookingHandler) HandleGetBooking(c *fiber.Ctx) error {
+	return nil
+}
