91ca057579eb20483dc02fbff85046abe6acec42
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-18 19:19:31 +0200

    [api_util] get auth util

---
+++ api/util.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hhanri/ghotel/types"
+)
+
+func GetAuth(ctx context.Context) (*types.User, error) {
+	user, ok := ctx.Value("user").(*types.User)
+	if !ok {
+		return nil, fmt.Errorf("Unauthorized")
+	}
+	return user, nil
+}
